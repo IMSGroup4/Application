2f791860308be1f7f8e7c662c5e043b8fffe0c74
Abbas <abbas.hathalia@gmail.com>
2023-04-15 15:55:48 +0200

    finished design of issue13

--- app/src/main/java/com/example/ulla_app/MainActivity.kt
+++ app/src/main/java/com/example/ulla_app/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.ulla_app
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
         setContentView(R.layout.main_activity)
     }
 }
