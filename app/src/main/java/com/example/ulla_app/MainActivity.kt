d9cd93dfa35daa18e61ffe3f538acdab0ffbf55c
Abbas <abbas.hathalia@gmail.com>
2023-04-15 19:02:41 +0200

    designed home activity

--- app/src/main/java/com/example/ulla_app/MainActivity.kt
+++ app/src/main/java/com/example/ulla_app/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.ulla_app
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    public override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         setContentView(R.layout.main_activity)
+
+        val getStartedBtn = findViewById<Button>(R.id.get_started_btn)
+        getStartedBtn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
 }
+
