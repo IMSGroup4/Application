e6f52af13f2d339918f51e04ab05168f68187e21
Abbas <abbas.hathalia@gmail.com>
2023-04-16 17:15:17 +0200

    declared different object

--- app/src/main/java/com/example/ulla_app/ObjectsActivity.kt
+++ app/src/main/java/com/example/ulla_app/ObjectsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ulla_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ObjectsActivity : AppCompatActivity() {
@@ -8,5 +11,14 @@ class ObjectsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         setContentView(R.layout.objects_activity)
+
+        val home_button = findViewById<Button>(R.id.home_button)
+        val map_button = findViewById<Button>(R.id.map_button)
+        val control_button = findViewById<Button>(R.id.control_button)
+        val title = findViewById<TextView>(R.id.object_title)
+        val image = findViewById<ImageView>(R.id.object_image)
+        val x_coor = findViewById<TextView>(R.id.x_coordinate)
+        val y_coor = findViewById<TextView>(R.id.y_coordinate)
+        val delete_btn = findViewById<Button>( R.id.delete_btn)
     }
 }
