50cb37001bc77b371b05421e928501ac3fb7f7a0
joelAndreasson <joelaron007@gmail.com>
2023-04-24 16:14:53 +0200

    Working map drawer
    Added gradle depencency to support JSON serialize

--- app/src/main/java/com/example/ulla_app/classes/MowerPathView.kt
+++ app/src/main/java/com/example/ulla_app/classes/MowerPathView.kt
@@ -9,23 +9,53 @@ import android.util.AttributeSet
 import android.view.View
 
 class MowerPathView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
-    private val robotPath = Path()
+    private val mowerPath = Path()
     private val lidarPoints = mutableListOf<Pair<Float, Float>>()
     private val robotPathPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.BLUE
-        strokeWidth = 5f
+        strokeWidth = 10f
         style = Paint.Style.STROKE
     }
     private val lidarPointPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.RED
         style = Paint.Style.FILL
     }
+    private var centerX = 0f
+    private var centerY = 0f
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        centerX = w / 2f
+        centerY = h / 2f
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawPath(robotPath, robotPathPaint)
+
+        canvas.save()
+        canvas.translate(centerX, centerY)
+
+        canvas.drawPath(mowerPath, robotPathPaint)
         for (point in lidarPoints) {
-            canvas.drawCircle(point.first, point.second, 3f, lidarPointPaint)
+            canvas.drawCircle(point.first, point.second, 20f, lidarPointPaint)
         }
+
+        canvas.restore()
+    }
+
+    fun updateMowerPosition(x: Float, y: Float) {
+        mowerPath.lineTo(x, y)
+        invalidate()
+    }
+
+    fun addLidarPoint(x: Float, y: Float) {
+        lidarPoints.add(Pair(x, y))
+        invalidate()
+    }
+
+    fun clear() {
+        mowerPath.reset()
+        lidarPoints.clear()
+        invalidate()
     }
 }
--- app/src/main/java/com/example/ulla_app/fragments/MapFragment.kt
+++ app/src/main/java/com/example/ulla_app/fragments/MapFragment.kt
@@ -6,20 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ulla_app.R
-import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
-import android.util.AttributeSet
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.ulla_app.classes.MowerPathView
+import com.example.ulla_app.dataclasses.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 class MapFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    var mowerPathView: MowerPathView? = null
+    val TAG = "MapFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,22 +34,57 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mowerPathView = view.findViewById<MowerPathView>(R.id.mowerPathView)
+        mowerPathView = view?.findViewById<MowerPathView>(R.id.mowerPathView)
 
+        val url = "https://ims-group4-backend.azurewebsites.net/api/positions"
 
-        
+        viewLifecycleOwner.lifecycleScope.launch {
+            val response = makeApiCall(url)
 
+            if (!response.isSuccessful) {
+                Log.e(TAG, "Error: ${response.code}")
+            }
+
+            val responseBodyStr = response.body?.string()
+            Log.d(TAG, "API Response: $responseBodyStr")
+
+            val positions: List<Position> = Json.decodeFromString(ListSerializer(Position.serializer()), responseBodyStr ?: "[]")
+            Log.d(TAG, "positions: $positions")
+
+            for(position in positions){
+                updateMowerPosition(position.x.toFloat(), position.y.toFloat())
+            }
+
+            //dummy data
+            addLidarPoint(210F, 100F)
+            addLidarPoint(240F, 130F)
+            addLidarPoint(260F, 150F)
+        }
     }
-    fun updateRobotPosition(x: Float, y: Float) {
-        mowerPathView.updateRobotPosition(x, y)
+
+    private fun updateMowerPosition(x: Float, y: Float) {
+        mowerPathView?.updateMowerPosition(x, y)
     }
 
-    fun addLidarPoint(x: Float, y: Float) {
-        mowerPathView.addLidarPoint(x, y)
+    private fun addLidarPoint(x: Float, y: Float) {
+        mowerPathView?.addLidarPoint(x, y)
     }
 
-    fun clear() {
-        mowerPathView.clear()
+    private fun clear() {
+        mowerPathView?.clear()
     }
+
+    private suspend fun makeApiCall(url: String): Response {
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            client.newCall(request).execute()
+        }
+    }
+
 
 }
