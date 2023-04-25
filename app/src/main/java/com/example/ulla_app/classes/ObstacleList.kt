a17e3732a638a62d9270574b8dd292bb69a94b38
sandranissan <sandranisssan@outlook.com>
2023-04-25 15:12:47 +0200

    Refactor ObstacleList-class, now possible to convert from Base64 to Bitmap format. Added PhotoInformation class

--- app/src/main/java/com/example/ulla_app/classes/ObstacleList.kt
+++ app/src/main/java/com/example/ulla_app/classes/ObstacleList.kt
@@ -1,23 +1,31 @@
 package com.example.ulla_app.classes
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import com.example.ulla_app.dataclasses.ObstaclePosition
+import android.util.Base64
 
 class ObstacleList {
-    private val list = mutableListOf<List<ObstaclePosition>>()
+    private val obstacleList = mutableListOf<ObstaclePosition>()
 
-    fun addObstacle(obstacle :List<ObstaclePosition>){
-        list.add(obstacle)
+    fun addObstacle(obstacle :ObstaclePosition){
+        obstacleList.add(obstacle)
     }
     fun removeObstacleAtIndex(index : Int){
-        list.removeAt(index)
+        obstacleList.removeAt(index)
     }
-    fun getObstacle(index : Int) : List<ObstaclePosition>{
-        return list[index]
+    fun getObstacle(index : Int) : ObstaclePosition{
+        return obstacleList[index]
     }
-    fun getObstacleList() : MutableList<List<ObstaclePosition>>{
-        return list
+    fun getObstacleList() : MutableList<ObstaclePosition>{
+        return obstacleList
     }
-    fun getObstacleImageRef(obstacleIndex : Int) : Int{
-        return list[obstacleIndex].first().photoRef
+    fun getObstacleImage(obstacleIndex: Int): Bitmap {
+        val imageBytes = Base64.decode(obstacleList[obstacleIndex].photoStringB64, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
+    fun getObstacleClassification(obstacleIndex : Int) : Pair<String,Float>{
+        return Pair<String, Float>(obstacleList[obstacleIndex].photoInformation.description, obstacleList[obstacleIndex].photoInformation.score)
+    }
+
 }
