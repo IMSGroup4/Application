41835e37a9c607509bd2e1cc583ce6e8f1fa9ff6
Abbas <abbas.hathalia@gmail.com>
2023-04-27 16:58:57 +0200

    cleand up the code i wrote before

--- app/src/main/java/com/example/ulla_app/fragments/ObstaclesFragment.kt
+++ app/src/main/java/com/example/ulla_app/fragments/ObstaclesFragment.kt
@@ -1,22 +1,15 @@
 package com.example.ulla_app.fragments
 
-import android.content.Context
-import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.ulla_app.R
 import com.example.ulla_app.classes.ObstacleList
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,63 +23,64 @@ private const val ARG_PARAM2 = "param2"
  */
 class ObstaclesFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    private lateinit var deleteBtn: FloatingActionButton
     private var param1: String? = null
     private var param2: String? = null
     private val obstacleList = ObstacleList()
 
-    val testData =  listOf(
-        mapOf(
-            "x" to "51.4463",
-            "y" to "6.6396",
-            "title" to "vase",
-            "img" to "@drawable/success.png"
-        ),
-        mapOf(
 
-            "x" to "51.3311",
-            "y" to "6.5616",
-            "title" to "dog",
-            "img" to "@drawable/error.png"
-        )
+    data class Obstacle(val title: String, val x: String, val y: String)
+    val obstacles = listOf(
+        Obstacle("Vase", "28.1215", "51.4512"),
+        Obstacle("Cat", "32.1254","21.4511"),
+        Obstacle("Cow", "25.1254","11.4515")
     )
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println(testData)
+       println(obstacles)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
 
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_obstacles, container, false)
+        return inflater.inflate(R.layout.obstacle_item, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val title = view.findViewById<TextView>(R.id.obstacle_title)
+        val x = view.findViewById<TextView>(R.id.x_coordinate)
+        val y = view.findViewById<TextView>(R.id.y_coordinate)
 
-        //This code is just for me to get started.
-        val scannedObject = view.findViewById<LinearLayout>(R.id.scanned_object)
-        val title = view.findViewById<TextView>(R.id.object_title)
-        val image = view.findViewById<ImageView>(R.id.object_image)
-        val x_coor = view.findViewById<TextView>(R.id.x_coordinate)
-        val y_coor = view.findViewById<TextView>(R.id.y_coordinate)
-        val delete_btn = view.findViewById<Button>( R.id.delete_btn)
 
-        title.text = testData[0].get("title").toString()
-        x_coor.text = testData[0].get("x").toString()
-        y_coor.text = testData[0].get("y").toString()
-
-        val draw = ContextCompat.getDrawable(requireContext(), R.drawable.success)
-        image.setImageDrawable(draw)
+        displayObstacle(title, x,y, obstacles)
     }
+
+    private fun displayObstacle(title: TextView, x: TextView,y: TextView,obstacles: List<Obstacle>){
+        obstacles.forEach { data->
+            title.text = data.title
+            x.text = data.x
+            y.text = data.y
+        }
+    }
+
+
+  /*  fun deleteObstacle(index: Int){
+        obstacleList.removeObstacleAtIndex(index)
+    }
+*/
 
     companion object {
         /**
